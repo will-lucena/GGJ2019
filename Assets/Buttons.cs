fe7014f58b8b3e61f76fd1da9baad3d5e8069708
fr4ct1ons <gabriellucena23@gmail.com>
2019-01-27 15:23:12 -0300

    Added Menu

---
+++ Assets/Buttons.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buttons : MonoBehaviour
+{
+
+    [SerializeField] GameObject credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadCredits()
+    {
+        credits.active = true;
+    }
+
+    public void LoadMenu()
+    {
+        credits.active = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
