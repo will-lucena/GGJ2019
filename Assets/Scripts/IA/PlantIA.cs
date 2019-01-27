6c72d86bae5297aa42545b828e05193cc28e746a
Will <will.lucen4@gmail.com>
2019-01-27 13:47:33 -0300

    Inimigo planta implementado

---
+++ Assets/Scripts/IA/PlantIA.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantIA : IA
+{
+    [Range(1, 3)]
+    [SerializeField] private float atk;
+    [SerializeField] private float launchForce;
+    [SerializeField] private Transform launchPoint;
+    [SerializeField] private GameObject bulletPrefab;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        PlantAttackBehaviour.notifyShoot += shoot;
+    }
+
+    private void shoot()
+    {
+        GameObject go = Instantiate(bulletPrefab);
+        Projectile projectile = go.GetComponent<Projectile>();
+        projectile.launch(launchForce, launchPoint, -1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            animator.SetBool("toAttack", true);
+        }
+    }
+}
